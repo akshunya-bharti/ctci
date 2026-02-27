7313d20d729f033607dd64ae1bfd92d26c19fb1e
Akshunya Bharti <akshu@live.in>
2026-02-27 22:59:42 +0530

    csharp 1.1 solved

--- problems/Ch01_Strings/01_IsUnique/csharp/Program.cs
+++ problems/Ch01_Strings/01_IsUnique/csharp/Program.cs
@@ -1,5 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-var line = Console.ReadLine();
-var n = int.Parse(line!);
-Console.WriteLine(n * n);
+internal class Program
+{
+    static List<char> charList = new List<char>();
+
+    private static void Main(string[] args)
+    {
+        var line = Console.ReadLine();
+        
+        foreach (var character in line)
+        {
+            if (IsParsedAlready(character))
+            {
+                Console.WriteLine("false");
+                return;
+            }
+        }
+
+        Console.WriteLine("true");
+    }
+
+    private static bool IsParsedAlready(char c)
+    {
+        if (charList.Count != 0)
+        {
+            if (charList.Contains(c))
+            {
+                return true;
+            }
+        }
+
+        charList.Add(c);
+        return false;
+    }
+}
