b79d1b0dfcb67bb3c3ee402284e8c5cf9261f56c
Akshunya Bharti <akshu@live.in>
2026-03-09 17:24:39 +0530

    csharp 1.3 solved

---
+++ problems/Ch01_Strings/03_URLify/csharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = Console.ReadLine();
+        var inputLength = Console.ReadLine();
+        Console.WriteLine(Urlify(input, Convert.ToInt32(inputLength)));
+    }
+
+    private static string Urlify(string input, int inputLength)
+    {
+        var output = new StringBuilder();
+        var iteratedLength = 0;
+
+        foreach(var inputChar in input)
+        {
+            iteratedLength += 1;
+
+            if (inputChar == ' ')
+                output.Append("%20");
+            else
+                output.Append(inputChar);
+            
+            if (iteratedLength == inputLength)
+                break;
+        }
+
+        return output.ToString();
+    }
+}
