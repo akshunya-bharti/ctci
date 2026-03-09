f74265c12ed41c1637bf7982a2f6ffee5b273a7d
Akshunya Bharti <akshu@live.in>
2026-03-09 16:46:18 +0530

    csharp 1.2 solved

---
+++ problems/Ch01_Strings/02_CheckPermutation/csharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var string1 = Console.ReadLine();
+        var string2 = Console.ReadLine();
+
+        string output = IsPermutation(string1, string2).ToString();
+        Console.WriteLine(output.ToLower());
+    }
+
+    private static bool IsPermutation(string string1, string string2)
+    {
+        if (string1.Length != string2.Length)
+            return false;
+
+        var string1Chars = string1.ToCharArray().ToList();
+
+        foreach(var character in string2)
+        {
+            var indexInString1 = string1Chars.IndexOf(character);
+            if (indexInString1 > -1)
+            {
+                string1Chars.RemoveAt(indexInString1);
+            }
+        }
+
+        if (string1Chars.Count > 0)
+            return false;
+
+        return true;
+    }
+}
